9eae68b7c8870102726368475bea29c98678c4ed
Felipe da Fonseca <felipesantos2606@outlook.com>
2022-11-15 21:02:36 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+
+        <header>
+            <div className="dsmeta-logo-container">
+            <img src={logo} alt="DSMeta" />
+            <h1>DSMeta</h1>
+            <p>
+                Powered by
+                <a href="https://github.com/Felipe-FSantos"> Felipe-FSantos </a>            
+            </p>
+            <p>
+            <a href="https://www.instagram.com/lipefs26"> Instagram @lipefs26</a>
+            </p>
+        </div>
+        </header>
+    )
+}
+
+export default Header
